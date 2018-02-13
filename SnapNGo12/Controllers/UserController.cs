a473b9afa43bc761e2327924c1223e4b8ed44725
Hannah Murphy <15827678+h-murphy@users.noreply.github.com>
2018-02-12 20:01:33 -0500

    initial commit

---
+++ SnapNGo12/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using SnapNGo12.Models;
+
+namespace SnapNGo12.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        //public Task<ActionResult> Register(User model)
+        public void Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+               var s = model.userName;
+                // var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                //var result = await UserManager.CreateAsync(user, model.Password);
+                //if (result.Succeeded)
+                //{
+                //  await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                // Send an email with this link
+                // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                // return RedirectToAction("Index", "Home");
+                ////}
+                // AddErrors(result);
+
+            }
+
+            // If we got this far, something failed, redisplay form
+            //return View();
+        }
+        public void PrintUserName()
+        {
+            Console.Write("Username");
+            
+        }
+    }
+}
